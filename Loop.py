02dac35cc98a4d633ca0b005f7209e640b1e4cba
Rahul Kumar <rahul305802@gmail.com>
2026-08-25 22:08:32 +0530

    Solve_Loops

---
+++ Loop.py
@@ -0,0 +1,60 @@
+# 1 to 10 print karo
+# 10 to 1 print karo
+# 1 to 20 even numbers
+# 1 to 20 odd numbers
+# 1 to 10 ka sum
+# 1 to 10 ka multiplication
+# Kisi number ka table
+# Number ke digits count karo
+
+
+# n = int(input("Enter a number: "))
+# for i in range(1,11,+1):
+#     print(i)
+
+
+
+# n = int(input("Enter a number: "))
+# for i in range(10,0,-1 ):
+#     print(i)
+
+
+# n = int(input("Enter a number: "))
+# for i in range(0,n,2):
+#     print(i)
+
+
+# n = int(input("Enter a number: "))
+# for i in range(1,n,2):
+#     print(i)
+
+
+# n = int(input("Enter a number: "))
+# sum = 0
+# for i in range(0,n,1):
+#     sum += i
+# print(sum)
+
+
+
+# n = int(input("Enter a number: "))
+# multi = 1
+# for i in range(1,n,1):
+#     multi *= i
+# print(multi)
+
+
+
+# n = int(input("Enter a number: "))
+
+# for i in range(1, 11, 1):
+#     print(i*n)
+
+
+# n = 12345
+# count = 0
+# while n > 0 :
+#     count+=1
+#     n = n // 10
+
+# print(count)
