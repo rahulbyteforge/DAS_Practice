c905dc0ecf81f02cdfd6537fc12a9cd60581b702
Rahul Kumar <rahul305802@gmail.com>
2026-08-26 21:11:28 +0530

    Solve List

---
+++ List.py
@@ -0,0 +1,134 @@
+# First element print karo
+# Last element print karo
+# Third element print karo
+# Second element change karo
+# Array ki length find karo
+# Ek new element add karo
+# Last element remove karo
+
+
+
+
+
+# 1
+
+# n = [1,2,3,4,5]
+# print(n[0])
+
+
+
+# n = [1,2,3,4,5]
+# for i in n:
+#     print(n[0])
+#     break
+
+
+# n = [1,2,3,4,5]
+# for i in range(len(n)):
+#     if i == 0:
+#         print(n[i])
+#         break
+
+
+
+# 2
+
+# n = [1,2,3,4,5]
+# print(n[4])
+
+
+# n = [1,2,3,4,5]
+# for i in n:
+#     print(n[4])
+#     break
+
+# n = [1,2,3,4,5]
+# for i in range(len(n)):
+#     if i == 4:
+#         print(n[i])
+
+
+#  3
+
+# n = [1,2,3,4,5]
+# print(n[2])
+
+# n =  [1,2,3,4,5]
+# for i in n:
+#     print(n[2])
+#     break
+
+# n = [1,2,3,4,5]
+# for i in range(len(n)):
+#     if i == 2:
+#         print(n[i])
+#         break
+
+
+
+
+#  4
+
+
+# n = [1,2,3,4,5]
+# n[1] = 6
+# print(n[1])
+
+
+# n = [1,2,3,4,5]
+# for i in n:
+#     n[1] = 6
+#     print(n)
+#     break
+
+# n = [1,2,3,4,5]
+# for i in range(len(n)):
+#     if i == 1:
+#         n[i] = 6
+# print(n)
+
+
+
+
+
+#  5
+
+
+
+# n = [1,2,3,4,5]
+# print(len(n))
+
+
+
+# n = [1,2,3,4,5]
+
+# for i in range(n):
+#     print(len(n))
+
+
+
+
+#  6
+
+# n = [1,2,3,4,5]
+# n.append(6)
+# print(n)
+
+
+
+# n = [1,2,3,4,5]
+# for i in range(len(n)):
+#     i=6
+# n.append(i)
+# print(n)
+
+
+
+
+
+#  7
+
+# n = [1,2,3,4,5]
+# n.pop(1)
+# print(n)
+
