e55782fa6ea11d35670c065046a02d23aa64e0fc
Rahul Kumar <rahul305802@gmail.com>
2026-09-16 09:12:18 +0530

    Solve prime number

---
+++ Prime.py
@@ -0,0 +1,16 @@
+def prime(n):
+
+    if n <= 1:
+        return False
+
+    for i in range(2, n):
+        if n % i == 0:
+            return False
+    return True
+
+n = int(input("Enter a number: "))
+
+if prime(n):
+    print("Prime")
+else:
+    print("Not Prime")
