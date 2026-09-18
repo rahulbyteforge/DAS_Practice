83c09212ea1278d1c0e470f2cb2d06238f46863c
Rahul Kumar <rahul305802@gmail.com>
2026-09-18 09:37:11 +0530

    Solve first ocurring index of string

---
+++ index_first_occurrence_in_String.py
@@ -0,0 +1,13 @@
+def STRstr(haystack, needle):
+
+    for i in range(len(haystack) - len(needle) + 1):
+        if haystack[i:i + len(needle)] == needle:
+            return i
+
+    return -1
+
+haystack = input("Enter a string: ")
+
+needle = input("Enter a needle: ")
+
+print(STRstr(haystack,needle))
