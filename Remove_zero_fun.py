8b2e7d5d3a072d643f06e96614b89a1fde6a3c87
Rahul Kumar <rahul305802@gmail.com>
2026-09-25 11:29:35 +0530

    Add files via upload

---
+++ Remove_zero_fun.py
@@ -0,0 +1,29 @@
+# 5. Move Zeroes to the End ⭐⭐
+
+# Concept: In-place manipulation / Two Pointers
+
+# Question:
+# Given an array, move all 0s to the end while maintaining the relative order of non-zero elements.
+
+# Example:
+
+# Input:  [0, 1, 0, 3, 12]
+
+# Output: [1, 3, 12, 0, 0]
+
+
+# def Zero(arr):
+#     j = 0
+#     for i in range(len(arr)):
+#         if arr[i] != 0:
+#             arr[i] ,arr[j] = arr[j] , arr[i]
+#             j+=1
+        
+#     return arr
+# arr = [0, 1, 0, 3, 12]
+# print(Zero(arr))
+
+
+
+
+    
